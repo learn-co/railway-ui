4f39dc25396b29928909c0fe016c7a6a739c6a09
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-12-10 18:02:35 -0500

    override phoenix router yikes

--- lib/railway_ui_web/router.ex
+++ lib/railway_ui_web/router.ex
@@ -25,23 +25,4 @@ defmodule RailwayUiWeb.Router do
     live "/published_messages", PublishedMessageLive.Index, session: [:current_user_uuid]
     live "/consumed_messages", ConsumedMessageLive.Index, session: [:current_user_uuid]
   end
-
-  def route_info(router, method, path, host) do
-    split_segments =
-      for segment <- String.split(path, "/"), segment != "", do: segment
-    case List.last(split_segments) do
-      last_segment
-      when last_segment in ["published_messages", "consumed_messages"] ->
-        do_route_info(router, method, [last_segment], host)
-      _ ->
-        do_route_info(router, method, split_segments, host)
-    end
-  end
-
-  def do_route_info(router, method, route_segments, host) do
-    case router.__match_route__(method, route_segments, host) do
-      {%{} = metadata, _prepare, _pipeline, {_plug, _opts}} -> Map.delete(metadata, :conn)
-      :error -> :error
-    end
-  end
 end
