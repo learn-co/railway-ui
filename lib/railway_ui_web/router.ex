edd787a5214afc3d6e2488aa965efe5eb4a17854
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-12-10 16:56:52 -0500

    override route_info to allow for forwarding

--- lib/railway_ui_web/router.ex
+++ lib/railway_ui_web/router.ex
@@ -25,4 +25,14 @@ defmodule RailwayUiWeb.Router do
     live "/published_messages", PublishedMessageLive.Index, session: [:current_user_uuid]
     live "/consumed_messages", ConsumedMessageLive.Index, session: [:current_user_uuid]
   end
+
+  def route_info(router, method, path, host) do
+    split_segments =
+      for segment <- String.split(path, "/"), segment != "", do: segment
+    route = split_segments |> List.last()
+    case router.__match_route__(method, [route], host) do
+      {%{} = metadata, _prepare, _pipeline, {_plug, _opts}} -> Map.delete(metadata, :conn)
+      :error -> :error
+    end
+  end
 end
