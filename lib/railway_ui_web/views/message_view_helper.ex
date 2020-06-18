0582fe527bffcfac9813cf284d7ffdf130aa0ebf
Lukeghenco <lukeghenco@gmail.com>
2020-06-17 19:26:26 -0700

    Pretty print a decoded version of the message

--- lib/railway_ui_web/views/message_view_helper.ex
+++ lib/railway_ui_web/views/message_view_helper.ex
@@ -1,5 +1,15 @@
 defmodule RailwayUiWeb.MessageViewHelper do
-  def decode_message_body(message_body) do
-    inspect(Jason.decode!(message_body))
+  def decode_message_body(%{message_type: message_type, encoded_message: encoded_message}) do
+    %{"encoded_message" => message_to_decode} = Jason.decode!(encoded_message)
+
+    try do
+      stuff = message_type
+      |> RailwayIpc.Core.Payload.module_from_type()
+      |> RailwayIpc.Core.Payload.decode_message(message_to_decode)
+      |> inspect(pretty: true)
+    rescue
+      UndefinedFunctionError -> "Unable to decode message due to missing message module"
+      error -> error
+    end
   end
 end
